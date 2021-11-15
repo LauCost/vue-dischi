9c251eb9cdf835a78f3f6fdc87e057df74f3255d
Laurentiu <lcostin40@gmail.com>
2021-11-15 12:19:46 +0100

    Finished the footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <footer>
+    <img src="../assets/Spotify_logo_without_text.svg" alt="" />
+  </footer>
 </template>
 
 
@@ -8,4 +10,13 @@
 
 
 <style lang="scss">
+footer {
+  background-color: #2e3a46;
+  width: 100%;
+
+  img {
+    width: 3%;
+    margin: 10px 20px;
+  }
+}
 </style>
