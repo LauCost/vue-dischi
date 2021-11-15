ea7c8a259e3c823c55dae9dc347bf7ab0b06e741
Laurentiu <lcostin40@gmail.com>
2021-11-15 15:00:23 +0100

    (BUG) Create code for call the api and print in DOM imgs and title

--- src/components/Content Main/ContDisks.vue
+++ src/components/Content Main/ContDisks.vue
@@ -1,20 +1,44 @@
 <template>
-  <div></div>
+  <div>
+    <Disk
+      v-for="disco in disks"
+      :key="disco.id"
+      :image="disco.poster"
+      :titolo="disco.title"
+    />
+    <!-- <div v-for="disk in disks" :key="disk.id">
+      <img :src="disk.poster" alt="" />
+      <h2>{{ disk.title }}</h2>
+    </div> -->
+  </div>
 </template>
 
 
 <script>
 import axios from "axios";
+import Disk from "./Disk.vue";
+
 export default {
+  data() {
+    return {
+      disks: [],
+    };
+  },
   mounted() {
     axios
       .get("https://flynn.boolean.careers/exercises/api/array/music")
       .then((r) => {
-        console.log(r);
+        console.log(r.data);
+
+        this.disks = r.data;
       })
       .catch((e) => {
         console.log(e, "ops");
       });
+  },
+
+  components: {
+    Disk,
   },
 };
 </script>
---
+++ src/components/Content Main/Disk.vue
@@ -0,0 +1,16 @@
+ <template>
+  <div class="disco">
+    <img :src="image" alt="" />
+    <p>{{ titolo }}</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    image: String,
+    titolo: String,
+  },
+};
+</script> 
