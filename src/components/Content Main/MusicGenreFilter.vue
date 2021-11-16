b53a24d97c29d9f512918cbdd87e44e70baa1775
Laurentiu <lcostin40@gmail.com>
2021-11-16 17:08:40 +0100

    Add style rules for filter section. Done the project (NO BONUS)

--- src/components/Content Main/MusicGenreFilter.vue
+++ src/components/Content Main/MusicGenreFilter.vue
@@ -1,16 +1,19 @@
 <template>
-  <select
-    v-model="filter"
-    @change="$emit('filter-genre', filter)"
-    name="type"
-    id="type"
-  >
-    <option value="All">All</option>
-    <option value="Rock">Rock</option>
-    <option value="Pop">Pop</option>
-    <option value="Jazz">Jazz</option>
-    <option value="Metal">Metal</option>
-  </select>
+  <div class="cont_filter">
+    <p>FILTER DISKS BY GENRE</p>
+    <select
+      v-model="filter"
+      @change="$emit('filter-genre', filter)"
+      name="type"
+      id="type"
+    >
+      <option value="All">All</option>
+      <option value="Rock">Rock</option>
+      <option value="Pop">Pop</option>
+      <option value="Jazz">Jazz</option>
+      <option value="Metal">Metal</option>
+    </select>
+  </div>
 </template>
 
 <script>
@@ -22,3 +25,23 @@ export default {
   },
 };
 </script>
+
+
+<style lang="scss">
+.cont_filter {
+  display: flex;
+  justify-content: center;
+  padding: 50px 0 0 0;
+
+  p {
+    color: white;
+    font-size: 40px;
+    margin-right: 20px;
+  }
+
+  select {
+    width: 5%;
+    font-size: 30px;
+  }
+}
+</style>
